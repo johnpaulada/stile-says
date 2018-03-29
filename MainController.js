1f271995814c76f69ab487e748e2dabc6dbf86eb
John Paul Ada <johnpaulada@gmail.com>
2018-03-30 00:14:00 +0800

    Basic game flow

--- MainController.js
+++ MainController.js
@@ -3,15 +3,17 @@ const Chance = require('chance')
 
 const MODE_CPU = 'CPU'
 const MODE_PLAYER = 'PLAYER'
+const CHAR_LIST = ['mafuyu', 'miu', 'kaho', 'maika']
 
 const chance = new Chance()
 
+const deepArrayEquals = (array1, array2) => {
+  return array1.length === array2.length
+    && array1.every((item1, index) => item1 === array2[index])
+}
+
 export default class extends Controller {
   static targets = [ "button" ]
-
-  intialize() {
-
-  }
   
   startGame() {
     this.sequence = [chance.integer({min: 0, max: 3})]
@@ -23,32 +25,57 @@ export default class extends Controller {
 
   lightEmUp() {
     if (this.currentIndex < this.sequence.length) {
+
       this.currentTarget = this.sequence[this.currentIndex]
+
       setTimeout(() => {
         this.lightUp(this.currentTarget)
       }, 1000)
+      
     } else {
       setTimeout(() => {
+
+        // Fade out previous target
         const previousTarget = this.buttonTargets[this.previousTarget]
         previousTarget.classList.remove('current')
+
         this.currentIndex = 0
-        this.data.set('mode', MODE_PLAYER)
+        this.mode = MODE_PLAYER
+        this.answers = []
       }, 1000)
     }
   }
 
   lightUp(index) {
+
+    // Fade out previous target
     const previousTarget = this.buttonTargets[this.previousTarget]
     previousTarget.classList.remove('current')
+
     const target = this.buttonTargets[index]
     target.classList.add('current')
+
     this.previousTarget = this.currentTarget
     this.currentIndex++
+
     this.lightEmUp()
   }
 
-  select() {
-
+  select(evt) {
+    if (this.mode === MODE_PLAYER) {
+      this.answers = this.answers + CHAR_LIST.findIndex(c => evt.target.id === c)
+      if (this.answers === this.sequence) {
+        if (this.sequence.length === 20) {
+          alert('yey you won!')
+        } else {
+          this.sequence = this.sequence + chance.integer({min: 0, max: 3})
+          this.currentIndex = 0
+          this.currentTarget = this.sequence[this.currentIndex]
+          this.previousTarget = this.currentTarget
+          this.lightEmUp()
+        }
+      }
+    }
   }
 
   get sequence() {
@@ -57,6 +84,26 @@ export default class extends Controller {
 
   set sequence(s) {
     this.data.set('sequence', s)
+
+    return this
+  }
+
+  get mode() {
+    return this.data.get('mode')
+  }
+
+  set mode(m) {
+    this.data.set('mode', m)
+
+    return this
+  }
+
+  get answers() {
+    return this.data.get('answers')
+  }
+
+  set answers(a) {
+    this.data.set('answers', a)
 
     return this
   }
